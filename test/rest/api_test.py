0c0267917b6caa7f0e3d24526a44054efbdf3bf1
Jeampier Carriel <jacarriel@telconet.ec>
2023-12-01 04:22:21 -0500

    proyecto test completo

---
+++ test/rest/api_test.py
@@ -0,0 +1,63 @@
+import http.client
+import os
+import unittest
+from urllib.request import urlopen
+
+import pytest
+
+BASE_URL = os.environ.get("BASE_URL")
+DEFAULT_TIMEOUT = 2  # in secs
+
+
+@pytest.mark.api
+class TestApi(unittest.TestCase):
+    def setUp(self):
+        self.assertIsNotNone(BASE_URL, "URL no configurada")
+        self.assertTrue(len(BASE_URL) > 8, "URL no configurada")
+
+    def test_api_add(self):
+        url = f"{BASE_URL}/calc/add/2/2"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+
+
+    def test_api_divide(self):
+        url = f"{BASE_URL}/calc/add/25/5"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+
+
+    def test_api_substract(self):
+        url = f"{BASE_URL}/calc/substract/25/5"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+
+
+    def test_api_multiply(self):
+        url = f"{BASE_URL}/calc/multiply/25/5"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+
+
+    def test_api_squareroot(self):
+        url = f"{BASE_URL}/calc/squareroot/81"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+
+
+    def test_api_logbase10(self):
+        url = f"{BASE_URL}/calc/logbase10/81"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
